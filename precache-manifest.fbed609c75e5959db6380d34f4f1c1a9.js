17ade37468d15b3a9a33dd51f4ce57ede93f4b0d
Suprayan Yapura <suprayan.yapura@gdn-commerce.com>
2021-01-09 01:38:43 +0700

    init web

---
+++ precache-manifest.fbed609c75e5959db6380d34f4f1c1a9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "96da16bc85c8d7f41999",
+    "url": "/css/app.ec8fdb3c.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "da505d75621baa204d9cea45220dc441",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3436ea3ac82482f3e3a0",
+    "url": "/js/about.063687bd.js"
+  },
+  {
+    "revision": "96da16bc85c8d7f41999",
+    "url": "/js/app.7e00ba43.js"
+  },
+  {
+    "revision": "11c8b73e15544d6f4373",
+    "url": "/js/chunk-vendors.8ea39674.js"
+  },
+  {
+    "revision": "7048f192e1433429f4289e8e21082100",
+    "url": "/manifest.json"
+  }
+]);
